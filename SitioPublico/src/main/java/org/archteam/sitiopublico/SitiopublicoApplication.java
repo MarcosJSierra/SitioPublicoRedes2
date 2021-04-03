e066ffc5308c8d174ed872eb3813b1f51c6ee4de
Marcos Sierra <sierramarcos146@gmail.com>
2021-04-02 21:12:04 -0600

    Agregando proyecto base

---
+++ SitioPublico/src/main/java/org/archteam/sitiopublico/SitiopublicoApplication.java
@@ -0,0 +1,13 @@
+package org.archteam.sitiopublico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitiopublicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitiopublicoApplication.class, args);
+	}
+
+}
---
+++ SitioPublico/src/test/java/org/archteam/sitiopublico/SitiopublicoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.archteam.sitiopublico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitiopublicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
