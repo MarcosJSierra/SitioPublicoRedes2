e7cbf383b2cd86d9b8f0dbf94cfb958d6e70832f
Marcos Sierra <sierramarcos146@gmail.com>
2021-04-02 21:58:44 -0600

    Agregar controller

---
+++ SitioPublico/src/main/java/org/archteam/sitiopublico/controller/base/base.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.archteam.sitiopublico.controller.base;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ *
+ * @author marcos
+ */
+@Controller
+public class base {
+    
+    @GetMapping("/")
+    public String Home(){
+        return "index";
+    }
+    
+    @GetMapping("/about")
+    public String pageAbout(){
+        return "base/about";
+    }
+    
+    @GetMapping("/ContactUs")
+    public String pageContact(){
+        return "base/contactUs";
+    }
+}
